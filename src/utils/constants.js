8cc822119529cbd2c71d43735ae5eee9cf743681
junaid <jaanusd79@gmail.com>
2024-03-24 03:56:39 +0530

    Updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const DATA_API = 'https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae'
+export const DATA_API = 'https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae';
